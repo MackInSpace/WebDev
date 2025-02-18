f96bfda3d310881a05dfe0a76e7187960f3430b6
Adam <amackey@lssu.edu>
2025-02-17 19:04:36 -0700

    bug fixing

--- 3-React/nucampsite/src/components/SubHeader.js
+++ 3-React/nucampsite/src/components/SubHeader.js
@@ -1,20 +1,20 @@
 import { Col, Row, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-const SubHeader = ({ current, detail}) => {
+const SubHeader = ({ current, detail }) => {
     return (
         <Row>
             <Col>
                 <Breadcrumb>
-                <BreadcrumbItem>
-                    <Link to='/'>Home</Link>
-                </BreadcrumbItem>
-                {detail && (
                     <BreadcrumbItem>
-                        <Link to='directory'>Directory</Link>
+                        <Link to='/'>Home</Link>
                     </BreadcrumbItem>
-                )}
-                <BreadcrumbItem active>{current}</BreadcrumbItem>
+                    {detail && (
+                        <BreadcrumbItem>
+                            <Link to='/directory'>Directory</Link>
+                        </BreadcrumbItem>
+                    )}
+                    <BreadcrumbItem active>{current}</BreadcrumbItem>
                 </Breadcrumb>
                 <h2>{current}</h2>
                 <hr />
