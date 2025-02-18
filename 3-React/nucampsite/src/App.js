329f42a59b25664f1d6032597792c91d287c0250
Adam <amackey@lssu.edu>
2025-02-17 17:28:44 -0700

    added :campsiteId for React Router functionality

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -6,6 +6,7 @@ import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import "./App.css";
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 //import { CAMPSITES } from './app/shared/CAMPSITES';
@@ -18,6 +19,10 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />}/>
         <Route path='directory' element={<CampsitesDirectoryPage />}/>
+        <Route 
+          path='directory/:campsiteId'
+          element={<CampsiteDetailPage />}
+        />
       </Routes>
       <Footer />
     </div>
