4ba8318e0f97668b28800c16be6b96533a8f4db3
MackInSpace <amackey@lssu.edu>
2025-03-10 16:51:59 -0600

    createAsyncThunk

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,6 +1,5 @@
-//import React from "react";
-//import { Container, Navbar, NavbarBrand } from "reactstrap";
-//import NucampLogo from "./app/assets/img/logo.png";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route} from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
@@ -10,9 +9,15 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AboutPage from './pages/AboutPage';
-//import { CAMPSITES } from './app/shared/CAMPSITES';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Header />
